4bf256a026d016d3d523e5d9a22feded403ee9ba
gegeqi <ccczzz1015@163.com>
2016-05-28 21:57:01 +0800

    Update separators.js

--- thousands_separators/src/separators.js
+++ thousands_separators/src/separators.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var a=function thousands_separators(input) {
+function thousands_separators(input) {
     var expectText = String(input).split(".");
     expectText[0] = expectText[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)','ig'),"$1,");
     return expectText.join(".");
